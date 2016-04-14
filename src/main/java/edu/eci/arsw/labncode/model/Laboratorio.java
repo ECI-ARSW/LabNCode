16f04c108bbb3b6279fb90c5884d922789aa1fb8
marialebu <mabu12@outlook.com>
2016-04-13 21:46:42 -0500

    Agregados métodos de laboratorios y clase para rest de laboratorios.

--- src/main/java/edu/eci/arsw/labncode/model/Laboratorio.java
+++ src/main/java/edu/eci/arsw/labncode/model/Laboratorio.java
@@ -20,4 +20,87 @@ public class Laboratorio {
     private Enunciado enunciado;
     private ArrayList<Sala> salas;
     private Materia materia;
+    
+    public Laboratorio(String nombre, Materia materia){
+        this.nombre = nombre; 
+        this.materia = materia; 
+        disponibilidad = true; 
+    }
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the fechaAct
+     */
+    public Date getFechaAct() {
+        return fechaAct;
+    }
+
+    /**
+     * @param fechaAct the fechaAct to set
+     */
+    public void setFechaActivacion(Date fechaAct) {
+        this.fechaAct = fechaAct;
+    }
+
+    /**
+     * @return the fechaDesc
+     */
+    public Date getFechaDesactivacion() {
+        return fechaDesc;
+    }
+
+    /**
+     * @param fechaDesc the fechaDesc to set
+     */
+    public void setFechaDesactivacion(Date fechaDesc) {
+        this.fechaDesc = fechaDesc;
+    }
+
+    /**
+     * @return the enunciado
+     */
+    public Enunciado getEnunciado() {
+        return enunciado;
+    }
+
+    /**
+     * @return the salas
+     */
+    public ArrayList<Sala> getSalas() {
+        return salas;
+    }
+
+    /**
+     * @param salas the salas to set
+     */
+    public void setSalas(ArrayList<Sala> salas) {
+        this.salas = salas;
+    }
+
+    /**
+     * @return the materia
+     */
+    public Materia getMateria() {
+        return materia;
+    }
+
+    /**
+     * @param materia the materia to set
+     */
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
 }
--- src/main/java/edu/eci/arsw/labncode/service/Servicio.java
+++ src/main/java/edu/eci/arsw/labncode/service/Servicio.java
@@ -5,13 +5,27 @@
  */
 package edu.eci.arsw.labncode.service;
 
+import edu.eci.arsw.labncode.model.Laboratorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author 2101240
  */
 public class Servicio {
+    @Autowired 
+    Laboratorio lab; 
+    
     //creacion de la sala
     //creacion de la laboratorio
+       
     //agregar sala al laboratorio
     //agregar estudiantes a la sala
     

678a9103eca1b92efe7006786d747191d93e4933
marialebu <mabu12@outlook.com>
2016-04-16 17:20:40 -0500

    Añadidos servicios

--- src/main/java/edu/eci/arsw/labncode/restcontroller/RestLaboratorio.java
+++ src/main/java/edu/eci/arsw/labncode/restcontroller/RestLaboratorio.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.labncode.service;
+package edu.eci.arsw.labncode.restcontroller;
 
 import edu.eci.arsw.labncode.model.Laboratorio;
 import edu.eci.arsw.labncode.model.ManejadorLaboratorio;
+import edu.eci.arsw.labncode.model.Persona;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author MaríaAlejandra
  */
 @RestController
-@RequestMapping("/rest/laboratorio") 
+@RequestMapping("/labncode/rest/") 
 /*
 El historial va mas orientado al profesor.Pensar en recursos y no en servicios
 
@@ -33,10 +34,16 @@ public class RestLaboratorio {
     @Autowired
     ManejadorLaboratorio labs;
     
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value="/profesor/", method = RequestMethod.GET)
     @ResponseBody
-    public List<Laboratorio> consords() {
-        return labs.getLaboratorios();
+    public List<Persona> consords() {
+        return labs.getProfesores();
+    }
+    
+    @RequestMapping(value="/profesor/{idProf}/", method = RequestMethod.GET)
+    @ResponseBody
+    public Persona consords(int idProf) {
+        return labs.getProfesor(idProf);
     }
     
     @RequestMapping(method = RequestMethod.POST)
--- src/main/java/edu/eci/arsw/labncode/restcontroller/Servicio.java
+++ src/main/java/edu/eci/arsw/labncode/restcontroller/Servicio.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.labncode.service;
+package edu.eci.arsw.labncode.restcontroller;
 
 
 import edu.eci.arsw.labncode.model.Estudiante;

c03b6f6eac777d3db43fb374cf16ae1d698752c1
marialebu <mabu12@outlook.com>
2016-04-17 21:22:31 -0500

    Cambio en el pom

--- src/main/java/edu/eci/arsw/labncode/model/ManejadorLaboratorio.java
+++ src/main/java/edu/eci/arsw/labncode/model/ManejadorLaboratorio.java
@@ -11,7 +11,6 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springseminar.model.ManejadorOrdenes;
 
 /**
  *
